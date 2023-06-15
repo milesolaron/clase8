60ad1d3889ea68fe0f0c45f8ab9542224b21de34
milesolaron <127025206+milesolaron@users.noreply.github.com>
2023-06-15 10:51:43 -0300

    Update validacion.js
    Incorporé las soluciones, haciendo que las funciones de validación sean utilizadas en el DOM y mejorando la función de validación de formularios y su gestión de errores.

--- tarea 1/validacion.js
+++ tarea 1/validacion.js
@@ -1,58 +1,59 @@
 const form = document.querySelector("#calculadora-edades");
 
+//Funcion que valida la cantidad de integrantes mediante distintos criterios
 function validarCantidadIntegrantes(cantidadIntegrantes) {
-    if (cantidadIntegrantes === "") {
-      return "El campo integrantes no puede estar vacío";
-    }
-    if (cantidadIntegrantes <= 1) {
-      return "La cantidad de integrantes debe ser más de uno";
-    }
-    if (cantidadIntegrantes > 10) {
-      return "La cantidad de integrantes máxima es 10";
-    }
-    if(!/^\d*$/.test(cantidadIntegrantes)) {
-      return "La cantidad de integrantes no puede ser expresada con decimales";
-    }
-    return "";
+  if (cantidadIntegrantes === "") {
+    return "El campo integrantes no puede estar vacío";
   }
-  
-  function validarEdades(edadIntegrante) {
-    if (edadIntegrante === "") {
-      return "Tenés que completar la edad de los integrantes";
-    }
-    if (edadIntegrante > 120) {
-      return "La edad máxima es 120 años";
-    }
-    if(!/^\d*$/.test(edadIntegrante)) {
-      return "La edad de los integrantes no puede ser expresada con decimales";
-    }
-    return "";
+  if (cantidadIntegrantes <= 1) {
+    return "La cantidad de integrantes debe ser más de uno";
   }
+  if (cantidadIntegrantes > 10) {
+    return "La cantidad de integrantes máxima es 10";
+  }
+  if (!/^\d*$/.test(cantidadIntegrantes)) {
+    return "La cantidad de integrantes no puede ser expresada con decimales";
+  }
+  return "";
+}
+//Funcion que valida las edades de los integrantes mediante distintos criterios
+function validarEdades(edadIntegrante) {
+  if (edadIntegrante === "") {
+    return "Tenés que completar las edades de los integrantes";
+  }
+  if (edadIntegrante > 120) {
+    return "La edad máxima es 120 años";
+  }
+  if (!/^\d*$/.test(edadIntegrante)) {
+    return "Las edades de los integrantes no pueden ser expresadas con decimales";
+  }
+  return "";
+}
 
+//Función que valida el formulario y se encarga de gestionar los posibles errores.
 function validarFormulario(validacion) {
-    const cantidadIntegrantes = form.integrantes.value;
-    const errorIntegrantes = validarCantidadIntegrantes(cantidadIntegrantes);
-  
-    const errores = {
-      integrantes: errorIntegrantes,
-    };
-  
-    gestionarErrores(errores, form.integrantes);
-  
-    if (validacion === "edadIntegrante") {
-      const edadIntegrante = form.edad;
-      const erroresEdades = [];
-  
-      for (let i = 0; i < edadIntegrante.length; i++) {
-        let errorEdad = validarEdades(edadIntegrante[i].value);
-        erroresEdades.push(errorEdad);
-        gestionarErrores({ edad: errorEdad }, form.edad[i]);
-      }
-    }
+  const cantidadIntegrantes = form.integrantes.value;
+  const errorIntegrantes = validarCantidadIntegrantes(cantidadIntegrantes);
+
+  const errores = {
+    integrantes: errorIntegrantes,
+  };
+
+  gestionarErrores(errores, form.integrantes);
+
+  if (validacion === "edadIntegrante") {
+    const edadIntegrante = form.edad.value;
+    const errorEdad = validarEdades(edadIntegrante);
+    gestionarErrores({ edad: errorEdad }, form.edad);
   }
-  function gestionarErrores(errores, item) {
-    const key = Object.keys(errores)[0];
-    const error = errores[key];
+}
+
+//Las funciones que validan los inputs son puestas en acción mediante la incorporación de un evento
+form.addEventListener("submit", function (event) {
+  event.preventDefault(); 
+  validarCantidadIntegrantes(form.integrantes.value);
+  validarEdades(form.edad.value);
+});
   
     if (error) {
       item.className = "error";
